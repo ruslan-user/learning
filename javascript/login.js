d83a5f22c6f78700648d4c086ff8c6e772c56120
ruslan-user <ostrenko.ruslan@ukr.net>
2018-04-12 17:32:47 +0300

    Admin-Login

--- javascript/login.js
+++ javascript/login.js
@@ -2,9 +2,31 @@ var usernameInput = document.getElementById("username") // Object
 var passwordInput = document.getElementById("password") // Object
 var button = document.getElementById("btn")
 
-var handleClick = function () {
-	console.log(usernameInput.value)
-	console.log(passwordInput.value)
+var handleClick = function (usernameInput, passwordInput, navigate) {
+	if (usernameInput == 'admin' && passwordInput == 'admin') {
+		navigate('pages/table.html');
+	}	else {
+			alert('Wrong username or password!');
+		}
+	console.log(usernameInput, passwordInput);
 }
 
 button.addEventListener('click', handleClick)
+
+
+var adminUser = {
+	username: 'admin',
+	password: 'admin'
+}
+
+
+function adminToStorage(adminUser) {
+	localStorage.setItem('adminUser', JSON.stringify(adminUser));
+}
+adminToStorage();
+
+
+var navigate = function (htmlFile) {
+ window.location.href = 'file:///C:/Users/ruffus/learning/' + htmlFile;
+};
+// navigate('pages/table.html');
