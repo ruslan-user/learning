a9d218380b6c8686aa6095bd8d36aba8ba31a438
ruslan-user <ostrenko.ruslan@ukr.net>
2018-04-26 15:34:28 +0300

    Test

---
+++ jstest1.js
@@ -0,0 +1,35 @@
+//JS Test #1
+
+	var count = 4;
+	console.log(count);
+
+
+	var users = ['Bob', 'Mike', 'Joey', 'Max', 'Sam'];
+
+
+
+	var logUsers =  function logUsers() {
+		for (i = [0]; i < 5; i++) {
+			var i = users[i];
+			console.log(users[]);
+		}
+	}
+
+
+
+	var logUserByIndex = function logUserByIndex(index) {
+		var index = users[];
+		console.log(index);
+
+	}
+
+	var logLastUser = function logLastUser(){
+
+	}
+	
+	
+	var actions = {
+		logUsers = logUsers(),
+		logUserByIndex = logUserByIndex(),
+		logLastUser = logLastUser()
+	}
